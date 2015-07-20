a0d62ca9e30c07f9728fd13ed3d6c161ca741bf5
Stuart Hutson <stu.hutson@gmail.com>
2015-07-20 23:41:23 +0100

    test changes

--- Assets/Scripts/Test.cs
+++ Assets/Scripts/Test.cs
@@ -2,22 +2,12 @@
 using System.Collections;
 
 public class Test : MonoBehaviour {
-
+    //test
 	// Use this for initialization
 	void Start () {
-        int a = 5;
-
-        switch (a)
+        for (int i = 0; i < 10; i++)
         {
-            case 1:
-                Debug.Log("A is 1");
-                break;
-            case 2:
-                Debug.Log("A is 2");
-                break;
-            default:
-                Debug.Log("A is not 1 nor 2");
-                break;
+            Debug.Log("Executed " + i + " times");
         }
 	
 	}
